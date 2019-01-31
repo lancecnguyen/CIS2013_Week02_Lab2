7554c4768545aec2c4c429b4cb4c6561c31e5d86
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2019-01-30 19:18:21 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "Week 2 Lab";
+	int bob;
+	int john;
+	
+	bob = 42;
+	john = 32;
+	int jim = bob + john;
+	short rachel = 10;
+	short luke = 7;
+	float pie = 3.14;
+	char middle = 'M';
+	cout << " Bob and John together are " << bob + john << " years old" << endl;
+	cout << middle << " is the middle initial for ";
+	cout << pie << " users that are " << luke;
+	cout << " year old." << endl;
+	cout << "Rachel is " << rachel << " years old and Luke is " << luke << ", they are both Bob's children.";
+	
+	return 0;
+}
